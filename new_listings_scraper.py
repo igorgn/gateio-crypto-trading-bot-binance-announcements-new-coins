08a8afa17c5fe694aa760e395e08165a504ffe9b
Igor Gnatyuk <igorgn@wix.com>
2021-11-13 14:20:38 +0200

    remove stub

--- new_listings_scraper.py
+++ new_listings_scraper.py
@@ -56,7 +56,7 @@ def get_last_coin():
      Returns new Symbol when appropriate
     """
     latest_announcement = get_announcement()
-    latest_announcement = 'Binance Will List MOVE (METIS)'
+
     found_coin = re.findall('\(([^)]+)', latest_announcement)
 
     # pull existing coin if file exists
